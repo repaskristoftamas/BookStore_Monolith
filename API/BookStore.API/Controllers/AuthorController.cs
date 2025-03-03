ceeeef0baaa3f78f5bc20e664aed78cfc8645751
Kristóf Répás <repas.kristof.tamas@protonmail.com>
2025-03-03 02:53:46 +0100

    Added GetAuthorByBookId()

--- API/BookStore.API/Controllers/AuthorController.cs
+++ API/BookStore.API/Controllers/AuthorController.cs
@@ -23,5 +23,20 @@ namespace BookStore.API.Controllers
 
             return Ok(_mapper.Map<IEnumerable<AuthorDto>>(result.Items));
         }
+
+        [HttpGet("by-book/{bookId}")]
+        public async Task<ActionResult<AuthorDto>> GetAuthorByBookId(int bookId)
+        {
+            try
+            {
+                var result = await _authorService.GetAuthorByBookIdAsync(bookId);
+
+                return Ok(_mapper.Map<AuthorDto>(result));
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
     }
 }
