05ef8da8e2775baf353b576c2cb6b92b9c82e33a
Kristóf Répás <repas.kristof.tamas@protonmail.com>
2025-03-03 22:09:46 +0100

    Added GetBookDetailsById(int) and CreateBook(BookForCreationDto)

--- API/BookStore.API/Controllers/BookController.cs
+++ API/BookStore.API/Controllers/BookController.cs
@@ -1,5 +1,4 @@
-﻿using AutoMapper;
-using BookStore.API.Models;
+﻿using BookStore.API.Models;
 using BookStore.API.QueryParameters;
 using BookStore.API.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -9,19 +8,44 @@ namespace BookStore.API.Controllers
 {
     [Route("api/books")]
     [ApiController]
-    public class BookController(IBookService bookService, IMapper mapper) : ControllerBase
+    public class BookController(IBookService bookService) : ControllerBase
     {
         private readonly IBookService _bookService = bookService ?? throw new ArgumentNullException(nameof(bookService));
-        private readonly IMapper _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<BookDto>>> GetBooks([FromQuery] BookQueryParameters bookQueryParameters)
         {
-            var result = await _bookService.GetBooks(bookQueryParameters);
+            var result = await _bookService.GetBooksAsync(bookQueryParameters);
 
             Response.Headers.Append("X-Pagination", JsonSerializer.Serialize(result.Pagination));
 
-            return Ok(_mapper.Map<IEnumerable<BookDto>>(result.Items));
+            return Ok(result.Items);
+        }
+
+        [HttpGet("{bookId}")]
+        public async Task<ActionResult<BookDto>> GetBookDetailsById(int bookId)
+        {
+            try
+            {
+                var result = await _bookService.GetBookDetailsByIdAsync(bookId);
+
+                return Ok(result);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<BookDto>> CreateBook(BookForCreationDto bookDto)
+        {
+            var createdBook = await _bookService.CreateBookAsync(bookDto);
+
+            return CreatedAtAction(nameof(GetBookDetailsById), new
+            {
+                bookId = createdBook.Id
+            }, createdBook);
         }
     }
 }
