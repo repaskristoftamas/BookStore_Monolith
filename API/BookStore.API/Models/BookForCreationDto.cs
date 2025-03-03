43fb31dfb36a2055a4e02194e74ce6d26af49877
Kristóf Répás <repas.kristof.tamas@protonmail.com>
2025-03-03 22:11:07 +0100

    Removed required attributes

--- API/BookStore.API/Models/BookForCreationDto.cs
+++ API/BookStore.API/Models/BookForCreationDto.cs
@@ -10,11 +10,7 @@ namespace BookStore.API.Models
 
         [MaxLength(2500)]
         public string? Description { get; set; }
-
-        [Required(ErrorMessage = "Book must have an author.")]
         public AuthorDto? Author { get; set; }
-
-        [Required(ErrorMessage = "Book must have a genre.")]
         public GenreDto? Genre { get; set; }
     }
 }
