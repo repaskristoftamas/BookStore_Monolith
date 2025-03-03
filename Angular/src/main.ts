60d64a0310bb4661ed20a5d04a4b21ea02943f9a
Kristóf Répás <repas.kristof.tamas@protonmail.com>
2025-03-03 02:58:27 +0100

    Added environments, main.ts, index.html, styles.css ang angular.json

--- Angular/src/main.ts
+++ Angular/src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
+import { FormsModule } from '@angular/forms';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    ...appConfig.providers,
+    importProvidersFrom(FormsModule)
+  ]
+})
   .catch((err) => console.error(err));
