91f0a1c23018bcbcf9d46a8f7a78feeb8710faed
Kristóf Répás <repas.kristof.tamas@protonmail.com>
2025-03-03 02:54:39 +0100

    Removed deconstructor

--- API/BookStore.API/QueryParameters/AuthorQueryParameters.cs
+++ API/BookStore.API/QueryParameters/AuthorQueryParameters.cs
@@ -2,16 +2,7 @@
 {
     public class AuthorQueryParameters : PaginationParameters
     {
-        public string? Name { get; set; }
-        public string? BookTitle { get; set; }
-        public string? SearchQuery { get; set; }
+        public string? Query { get; set; }
         public string? OrderBy { get; set; } = "name";
-
-        public void Deconstruct(out string? name, out string? bookTitle, out string? searchQuery)
-        {
-            name = Name;
-            bookTitle = BookTitle;
-            searchQuery = SearchQuery;
-        }
     }
 }
