c5b97e401d8599076323c38faa88fba635991bd5
Kristóf Répás <repas.kristof.tamas@protonmail.com>
2025-03-26 16:08:33 +0100

    Added service methods to support GetDetailsById and Create methods

--- API/BookStore.API/Services/BookService.cs
+++ API/BookStore.API/Services/BookService.cs
@@ -1,6 +1,5 @@
 ﻿using AutoMapper;
 using BookStore.API.Entities;
-using BookStore.API.Helpers;
 using BookStore.API.Models;
 using BookStore.API.QueryParameters;
 using BookStore.API.Repositories;
@@ -10,17 +9,19 @@ namespace BookStore.API.Services
 {
     public interface IBookService
     {
-        Task<PagedResult<BookDto>> GetBooksAsync(FilterOptions filterOptions);
+        Task<PagedResult<BookDto>> GetBooksAsync(BookFilterOptions filterOptions);
         Task<BookDto?> GetBookDetailsByIdAsync(int bookId);
         Task<BookDto> CreateBookAsync(BookForCreationDto bookDto);
     }
 
-    public class BookService(IBookRepository bookRepository, IMapper mapper) : IBookService
+    public class BookService(IBookRepository bookRepository, IAuthorRepository authorRepository, IGenreRepository genreRepository, IMapper mapper) : IBookService
     {
         private readonly IBookRepository _bookRepository = bookRepository ?? throw new ArgumentNullException(nameof(bookRepository));
+        private readonly IAuthorRepository _authorRepository = authorRepository ?? throw new ArgumentNullException(nameof(authorRepository));
+        private readonly IGenreRepository _genreRepository = genreRepository ?? throw new ArgumentNullException(nameof(genreRepository));
         private readonly IMapper _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
 
-        public async Task<PagedResult<BookDto>> GetBooksAsync(FilterOptions filterOptions)
+        public async Task<PagedResult<BookDto>> GetBooksAsync(BookFilterOptions filterOptions)
         {
             //ValidationHelper.ValidatePagination(filterOptions.PageNumber, filterOptions.PageSize);
 
@@ -52,34 +53,38 @@ namespace BookStore.API.Services
         {
             var book = _mapper.Map<Book>(bookDto);
 
-            if (book.Author is null) throw new InvalidOperationException("Author is required.");
-
-            var existingAuthor = await _bookRepository.GetAuthorByNameAsync(book.Author.Name);
-
-            if (existingAuthor is null)
-            {
-                existingAuthor = new Author { Name = book.Author.Name };
-                await _bookRepository.AddAuthorAsync(existingAuthor);
-            }
-
-            book.Author = existingAuthor;
+            book.Author = book.Author is null ? throw new InvalidOperationException("Author is required.") : await GetOrCreateAuthorAsync(book.Author);
 
             if (book.Genre is not null)
-            {
-                var existingGenre = await _bookRepository.GetGenreByNameAsync(book.Genre.Name);
-
-                if (existingGenre is null)
-                {
-                    existingGenre = new Genre { Name = book.Genre.Name };
-                    await _bookRepository.AddGenreAsync(existingGenre);
-                }
-
-                book.Genre = existingGenre;
-            }
+                await GetOrCreateGenreAsync(book.Genre);
 
             var result = await _bookRepository.CreateBookAsync(book);
 
             return _mapper.Map<BookDto>(result);
+        }
+
+        private async Task<Author> GetOrCreateAuthorAsync(Author author)
+        {
+            var existingAuthor = await _authorRepository.GetAuthorByNameAsync(author.Name);
+
+            if (existingAuthor is not null) return existingAuthor;
+
+            var newAuthor = new Author { Name = author.Name };
+            await _authorRepository.CreateAuthorAsync(newAuthor);
+
+            return newAuthor;
+        }
+
+        private async Task<Genre> GetOrCreateGenreAsync(Genre genre)
+        {
+            var existingGenre = await _genreRepository.GetGenreByNameAsync(genre.Name);
+
+            if (existingGenre is not null) return existingGenre;
+
+            var newGenre = new Genre { Name = genre.Name };
+            await _genreRepository.CreateGenreAsync(genre);
+
+            return newGenre;
         }
     }
 }
